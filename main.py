2a3c1327654d342ced0f5e80a473895096ba57fd
anAwesomeWave <timofey.zubov.05@gmail.com>
2022-11-06 02:22:28 +0300

    Write the first version of function 'get_all_torrents', that retrieve all information about torrents on the specific page.(without link to torrent-file!) Написал функцию, надо добавить в нее ссылку на торрент-файл и, возможно, ее переделать вообще.(и сделать добавление торренотов в словарь со всех страниц!)

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import traceback
+import urllib.parse
 import sys
 
 import requests
 from bs4 import BeautifulSoup
+
+from re import sub
 
 import logging
 
@@ -11,18 +15,98 @@ log.setLevel(logging.INFO)
 log.addHandler(logging.FileHandler('image.log'))
 
 
-#url = 'https://rutracker.org/forum/tracker.php?nm=peaky%20blinders%202'
+def get_all_torrents(cont): # return list of dicts
+    arr = []
+    for i in cont.find_all('tr'):
+        d = dict()
 
+        # for i in [('is_approved','find_all('td', {'class': 'row1 t-ico'})[1]['title']')]
+        # TODO: может сработать для всего, кроме is_approved (цикл для всех остальных соответственно, переписать)
+        # approved or not
+        try:
+            d['is_approved'] = i.find_all('td', {'class': 'row1 t-ico'})[1]['title'] # or 'status'
+        except Exception as e:
+            logging.error("is_approved-DICT - " + traceback.format_exc())
+            log.info("is_approved-DICT - " + traceback.format_exc())
+            d['is_approved'] = None
 
-#headers = {}#'bb-webext': '{"auid":"VGssRNyL7mxf","browser":"chrome-105","proxy":true,"version":"0.9.28"}',
-           #'Cookie': 'bb_guid=9jBTHH8Ud4vl; bb_ssl=1; bb_session=0-44811045-5w6kyGfPV9rXTKsXU7Gm; _ym_uid=1658581846128378810; _ym_d=1660744038; _ym_isad=1'}
+        # forum
+        try:
+            d['forum'] = i.select('.row1.f-name-col')[0].find('a').string # by css selector
+        except Exception as e:
+            logging.error('forum-DICT - ' + traceback.format_exc())
+            log.info('forum-DICT - ' + traceback.format_exc())
+            d['forum'] = None
 
+        # filename
+        # хочу брать весь текст
+        # включая возмодные теги span
+        # а потом, все, что в <> удалять с помощью regex возможно или циклом просто
+        # sub(r'<[^<>]*>', str)
+        # по итогу не пришлось даже)
 
-#r = requests.get(url, headers=headers)
+        try:
+            #d['filename'] = i.find('td', {'class': 'row4 med tLeft t-title-col tt'}).find('a').string
+            d['filename'] = i.select('.row4.med.tLeft.t-title-col.tt')[0].find('a').string
+        except Exception as e:
+            logging.error('forum-FILENAME - ' + traceback.format_exc())
+            log.info('forum-FILENAME - ' + traceback.format_exc())
+            d['filename'] = None
+
+        # author
+        try:
+            d['author'] = i.select('.row1.u-name-col')[0].find('a').string
+        except Exception as e:
+            logging.error('forum-AUTHOR - ' + traceback.format_exc())
+            log.info('forum-AUTHOR - ' + traceback.format_exc())
+            d['author'] = None
+
+        # size TODO: убрать лишнее из строки
+        try:
+            d['size'] = i.select(".row4.small.nowrap.tor-size")[0].find('a').string
+        except Exception as e:
+            logging.error('forum-SIZE - ' + traceback.format_exc())
+            log.info('forum-SIZE - ' + traceback.format_exc())
+            d['size'] = None
+
+        # seeds
+        try:
+            # d['seeds'] = i.select('.row4.nowrap')[1].find('b').string
+            d['seeds'] = i.find('b').string
+        except Exception as e:
+            logging.error('forum-SEEDS - ' + traceback.format_exc())
+            log.info('forum-SEEDS - ' + traceback.format_exc())
+            d['seeds'] = None
+
+        # leeches
+        try:
+            d['leeches'] = i.select('.row4.leechmed.bold')[0].string
+        except Exception as e:
+            logging.error('forum-LEECHES - ' + traceback.format_exc())
+            log.info('forum-LEECHES - ' + traceback.format_exc())
+            d['leeches'] = None
+
+        # downloads
+        try:
+            d['downloads'] = i.select('.row4.small.number-format')[0].string
+        except Exception as e:
+            logging.error('forum-DOWNLOADS - ' + traceback.format_exc())
+            log.info('forum-DOWNLOADS - ' + traceback.format_exc())
+            d['downloads'] = None
+        # date
+        try:
+            d['date'] = i.find('p').string
+        except Exception as e:
+            logging.error('forum-DATE - ' + traceback.format_exc())
+            log.info('forum-DATE - ' + traceback.format_exc())
+            d['date'] = None
+        arr.append(d)
+    print(arr)
+    return arr
+
 
 
 login_url = 'https://rutracker.org/forum/login.php'
-
 
 # get login and password from rutracker
 f = open('keys.txt', 'r')
@@ -43,18 +127,28 @@ except requests.exceptions.ConnectionError:
     log.info('CANNOT LOG-IN: connection error')
     sys.exit()
 
-soup = BeautifulSoup(r.content, 'html.parser')
-
-
-for c in r.cookies:
-    print(c.name, c.value)
-user_input = 'peaky%20blinders%202'  # base64
+# for c in r.cookies:
+#     print(c.name, c.value)
+user_input = urllib.parse.quote("peaky blinders 2")  # Percent-encoding
 
 # get all matches from rutracker
 # pass cookie to authenticate
 user_request = requests.get(f'https://rutracker.org/forum/tracker.php?nm={user_input}', cookies=r.cookies).content
 
-soup = BeautifulSoup(user_request, 'html.parser') #  .prettify()
+soup = BeautifulSoup(user_request, 'html.parser')  # .prettify()
 
-container = soup.find('div', {"id": "search-results"})
-print(container.prettify())
+container = soup.find('div', {"id": "search-results"}).find('tbody')
+all_torrents = get_all_torrents(container)  # list of dicts (all torents)
+# name, size, link, date, author, seeds, leechers
+
+# TODO: iterate through all pages
+# class bottom_info -> class nav -> class pg-jump-menu -> find_all('a' class pg)
+# request к каждой
+
+
+#print(container.prettify())
+
+
+# мб завернуть все в класс и сделать функции
+# типа api
+# а потом в других скриптах уже пользоваться
